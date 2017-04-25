a42cd5ec2485dee623c4e58a7cf43a702d2ddda8
sirrah23 <hseth93@gmail.com>
2017-04-25 00:37:00 -0400

    Can draw sphere now

--- ray.py
+++ ray.py
@@ -16,10 +16,21 @@ class Ray(object):
     def normalize(vec):
         return vec/LA.norm(vec)
 
-    def color(self):
-        unit_direction = Ray.normalize(self.direction)
-        t = 0.5*unit_direction[1] + 1.0
-        return (1.0 - t)*np.ones(3) + t*np.array([0.5, 0.7, 1.0])
+def color(ray):
+    if hit_sphere(np.array([0, 0, -1]), 0.5, ray):
+        return np.array([1, 0, 0])
+    unit_direction = Ray.normalize(ray.direction)
+    t = 0.5*unit_direction[1] + 1.0
+    return (1.0 - t)*np.ones(3) + t*np.array([0.5, 0.7, 1.0])
+
+def hit_sphere(center, radius, ray):
+    """Determine if a ray hits a sphere with center and radius"""
+    oc = ray.origin - center
+    a = np.dot(ray.direction, ray.direction)
+    b = 2.0 * np.dot(oc, ray.direction)
+    c = np.dot(oc, oc) - radius**2
+    discriminant = b**2 - 4*a*c
+    return discriminant > 0
 
 
 def main():
@@ -36,7 +47,7 @@ def main():
             u = float(i) / float(nx)
             v = float(j) / float(ny)
             r = Ray(origin, lower_left_corner + u * horizontal + v*vertical)
-            col = r.color()
+            col = color(r)
             ir = int(255.99 * col[0])
             ig = int(255.99 * col[1])
             ib = int(255.99 * col[2])
